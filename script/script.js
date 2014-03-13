c18c55efb98a82635ba9bfcfb37316cdf97a5eb0
Sarah Stancik <skstancik@utexas.edu>
2014-03-12 22:24:41 -0500

    single page style

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function onHover(this) {
+	document.getElementById("facebook").style.src = "images/facebookHover.png";
+}
+
+function unHover(this) {
+	document.getElementById("facebook").style.src = "images/facebook.png";
+}
