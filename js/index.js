fe1420ac510826f99c1b6b2d97afebfa266162c7
Sarah Stancik <skstancik@gmail.com>
2017-01-13 18:38:06 -0500

    jquery for animation pause on mobile

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@ $('#toggle').click(function() {
 	$(this).toggleClass('active');
 	$('#overlay').toggleClass('open');
 });
+
+$(document).ready(function() {
+    $('.hover_mobile').bind('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
