3250d91021c81b3e2c40c625b9fcd2ecad6f9972
Sarah Stancik <skstancik@gmail.com>
2017-01-13 18:25:54 -0500

    rm active and focus pseudo-classes from box-overlay

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 $('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
-  });
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+});
